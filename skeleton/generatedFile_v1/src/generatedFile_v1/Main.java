c076ce809085ef88bfa2b218b5318b072de5810a
Hege <dani940414@gmail.com>
2016-03-28 21:24:55 +0200

    move szekvencia implementálása skeletonba

--- skeleton/generatedFile_v1/src/generatedFile_v1/Main.java
+++ skeleton/generatedFile_v1/src/generatedFile_v1/Main.java
@@ -1,6 +1,8 @@
 package generatedFile_v1;
 
+import java.io.Console;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by hege on 2016.03.28..
@@ -9,10 +11,9 @@ public class Main {
 
     public static void main(String args[]) {
 
+        System.out.println("Initialization sequence: ");
         Level level = new Level();
-        level.load();
-
-        System.out.println("Initialization sequence is done.");
+        System.out.println("Initialization sequence is done.\n");
 
         System.out.println("Please select an option!");
         //System.out.println("[0]: Init sequence");
@@ -22,25 +23,37 @@ public class Main {
         System.out.println("[4]: Missile sequence");
         System.out.println("[5]: Reset sequence");
         System.out.println("[6]: End of game sequence");
-        System.out.println("[x]: exit");
+        System.out.println("[0]: exit\n");
 
-        char input = '0';
+        int input = 10000;
+        Scanner in = new Scanner(System.in);
 
-        while (input != 'x'){
-            try {
-                input = (char) System.in.read();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        while (input != 0){
+
+            input = in.nextInt();
+
         	switch(input) {
-	    		case '1':
-                    ONeill oneill = new ONeill(level.ls.get(0), Direction.Bottom);
-	    		case '2': testBox();
-	    		case '3': testDoor();
-	    		case '4': testMissile();
-	    		case '5': testReset();
-	    		case '6': testEndOfGame();
-	    		case 'x': System.exit(0);	
+	    		case 1:
+                    Floor f1 = new Floor(level, false);
+                    Floor f2 = new Floor(level, false);
+                    f1.setNeighbour(Direction.Bottom, f2);
+                    f2.setNeighbour(Direction.Top, f1);
+                    ONeill oneill = new ONeill(f1, Direction.Bottom);
+                    oneill.move();
+	    		case 2:
+                    //oneill.boxing();
+                    break;
+	    		case 3:
+                    break;
+	    		case 4:
+                    break;
+	    		case 5:
+                    break;
+	    		case 6:
+                    break;
+	    		case 0: System.exit(0);
+                default:
+                    break;
         	}
         }
     }
