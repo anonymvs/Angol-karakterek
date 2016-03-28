b1006052d3bc2494a98532c887872224508dc80f
Hege <dani940414@gmail.com>
2016-03-28 18:25:15 +0200

    minor fixes in skeleton's main.java

--- skeleton/generatedFile_v1/src/generatedFile_v1/Main.java
+++ skeleton/generatedFile_v1/src/generatedFile_v1/Main.java
@@ -1,5 +1,7 @@
 package generatedFile_v1;
 
+import java.io.IOException;
+
 /**
  * Created by hege on 2016.03.28..
  */
@@ -10,8 +12,7 @@ public class Main {
     	
         Level level = new Level();
         level.load();
-        
-    	char o;
+
         System.out.println("Please select an option!");
         System.out.println("[0]: Init sequence");
         System.out.println("[1]: Move sequence");
@@ -21,10 +22,16 @@ public class Main {
         System.out.println("[5]: Reset sequence");
         System.out.println("[6]: End of game sequence");
         System.out.println("[x]: exit");
-        
-        for(int i = 0; o != 'x'; i++){
-        	o = (char) System.in.read();
-        	switch(o){
+
+        char input = 'z';
+
+        while (input != 'x'){
+            try {
+                input = (char) System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        	switch(input){
 	    		case '0': testInit();
 	    		case '1': testMove();
 	    		case '2': testBox();
